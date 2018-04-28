887933e9933247d121c210730f8031274e162749
Alex Manning <alexandersmanning@gmail.com>
2018-04-27 18:26:53 -0700

    Updated post and started session mocking

--- app/models/post_test.go
+++ app/models/post_test.go
@@ -5,7 +5,6 @@ import (
 )
 
 func clearPosts(t *testing.T) {
-	//db := database.GetStore()
 	_, err := db.Query("DELETE FROM posts")
 
 	if err != nil {
@@ -60,35 +59,87 @@ func TestPostCreation(t *testing.T) {
 }
 
 func TestCreatePost(t *testing.T) {
-	clearPosts(t)
-	//db := database.GetStore()
-	_, err := db.Query(
-		"INSERT INTO posts (body, title) VALUES" +
-			"('body_1', 'title_1')," +
-			"('body_2', 'title_2')")
+	t.Run("Handle posts without associated user", func(t *testing.T) {
+		clearPosts(t)
+		_, err := db.Query(
+			`INSERT INTO posts (body, title) VALUES
+			('body_1', 'title_1'),
+			('body_2', 'title_2')`)
 
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	posts, err := db.AllPosts()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Run("Returns the correct number of elements", func(t *testing.T) {
-		if len(posts) != 2 {
-			t.Errorf("Expected 2 posts, got %d", len(posts))
+		if err != nil {
+			t.Fatal(err)
 		}
+
+		posts, err := db.AllPosts()
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Run("Returns the correct number of elements", func(t *testing.T) {
+			if len(posts) != 2 {
+				t.Errorf("Expected 2 posts, got %d", len(posts))
+			}
+		})
+
+		t.Run("Elements container the correct data", func(t *testing.T) {
+			if post := posts[0]; post.Body != "body_1" {
+				t.Errorf("Incorrect body, expected 'body_1', got %s", post.Body)
+			}
+
+			if post := posts[1]; post.Title != "title_2" {
+				t.Errorf("Incorrect body, expected 'title_2', got %s", post.Body)
+			}
+		})
 	})
 
-	t.Run("Elements container the correct data", func(t *testing.T) {
-		if post := posts[0]; post.Body != "body_1" {
-			t.Errorf("Incorrect body, expected 'body_1', got %s", post.Body)
+	t.Run("Handles posts with associated user", func(t *testing.T) {
+		clearPosts(t)
+		clearUsers(t)
+
+		rows, err := db.Query(`
+			INSERT INTO users (email) VALUES ('email@fake.com') RETURNING id
+		`)
+
+		var userID int
+
+		if err != nil {
+			t.Fatal(err)
 		}
 
-		if post := posts[1]; post.Title != "title_2" {
-			t.Errorf("Incorrect body, expected 'title_2', got %s", post.Body)
+		for rows.Next() {
+			if err := rows.Scan(&userID); err != nil {
+				t.Fatal(err)
+			}
 		}
+
+		_, err = db.Query(`
+			INSERT INTO posts (user_id, title, body) VALUES ($1, $2, $3)
+		`, userID, "title_3", "body_3")
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		posts, err := db.AllPosts()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Run("Returns the correct number of elements", func(t *testing.T) {
+			if len(posts) != 1 {
+				t.Errorf("Expected %d posts, got %d", 1, len(posts))
+			}
+		})
+
+		t.Run("Elements have the correct User and Body", func(t *testing.T) {
+			post := posts[0]
+			if post.Author.Email != "email@fake.com" {
+				t.Errorf("Expected user email to be %s, got %s", "email@fake.com", post.Author.Email)
+			}
+
+			if post.Body != "body_3" {
+				t.Errorf("Expected body to be %s, got %s", "body_3", post.Body)
+			}
+		})
 	})
+
 }
