ed7d4014de345432425d9c7d261282531ef20a72
Alex Manning <alexandersmanning@gmail.com>
2018-06-05 21:15:10 -0700

    Addition of user session and user tests

--- app/database/user_session_test.go
+++ app/database/user_session_test.go
@@ -6,6 +6,32 @@ import (
 	"reflect"
 )
 
+func createTestSession(userId int, t *testing.T) models.UserSession {
+	token, err := CreateSessionToken()
+	if err != nil {
+		t.Fatal(err)
+	}
+	rows, err := db.Query(`
+		INSERT INTO user_sessions (user_id, token)
+		VALUES ($1, $2) RETURNING ID
+	`, userId, token)
+
+	defer rows.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var id int
+	for rows.Next() {
+		if err := rows.Scan(&id); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	return models.UserSession{Id: id, User: models.User{Id: userId}, SessionToken: token }
+}
+
 func TestCreateUserSession(t *testing.T) {
 	clearUsers(t)
 
@@ -29,4 +55,51 @@ func TestCreateUserSession(t *testing.T) {
 	if us.SessionToken == "" {
 		t.Errorf("Expected a token for the user session, got nothing")
 	}
+}
+
+func TestGetUserBySessionToken(t *testing.T) {
+	clearUsers(t)
+
+	uOne := models.User{Email: "email1@fake.com"}
+	id := createTestUser(&uOne, t)
+	uOne.Id = id
+	usOne := createTestSession(id, t)
+
+	t.Run("It returns the user when one user entry exists", func(t *testing.T) {
+		user, err := db.GetUserBySessionToken(usOne.User.Id, usOne.SessionToken)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !reflect.DeepEqual(user, uOne) {
+			t.Errorf("Expected %v to equal %v", user, uOne)
+		}
+	})
+
+	t.Run("It returns an empty user if nothing can be found", func(t *testing.T) {
+		user, err := db.GetUserBySessionToken(usOne.User.Id, "non-existent-token")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if user.Id != 0 || user.Email != "" {
+			t.Errorf("Expected no user to be found, a user with id %d and email %s was returned", user.Id, user.Email)
+		}
+	})
+
+	t.Run("Multiple entries", func(t *testing.T) {
+		uTwo := models.User{Email: "email2@fake.com"}
+		idTwo := createTestUser(&uTwo, t)
+		uTwo.Id = idTwo
+		usTwo := createTestSession(idTwo, t)
+
+		user, err := db.GetUserBySessionToken(usTwo.User.Id, usTwo.SessionToken)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !reflect.DeepEqual(user, uTwo) {
+			t.Errorf("Expected %v to equal %v", user, uTwo)
+		}
+	})
 }
